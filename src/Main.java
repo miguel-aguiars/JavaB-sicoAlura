44d88a3d000a9a4c844bfd8047b3efb0b0e2f743
Miguel Aguiar <miguel_aguiar2016@outlook.com>
2023-03-21 18:19:42 -0300

    upload de arquivos do curso

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] idades = {12, 56, 39, 58} ;
+
+        for (int idade : idades) {
+            System.out.println(idade);
+
+        }
+
+        double wage = 2705.22;
+        System.out.println(wage);
+
+        int wageToInt = (int) wage;
+        System.out.println(wageToInt);
+
+        int numeroGrande = 2000000000;
+
+
+    }
+}
